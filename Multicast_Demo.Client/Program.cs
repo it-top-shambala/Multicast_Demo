22b5a09e75c057da6921169d6a21348437bb6c00
Starinin Andrey <starininandrey@gmail.com>
2022-12-07 19:50:42 +0300

    Init

---
+++ Multicast_Demo.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+ 
+using var udpClient = new UdpClient(8001);
+var broadcastAddress = IPAddress.Parse("235.5.5.11");
+udpClient.JoinMulticastGroup(broadcastAddress);
+Console.WriteLine("Начало прослушивания сообщений");
+while (true)
+{
+    var result = await udpClient.ReceiveAsync();
+    var message = Encoding.UTF8.GetString(result.Buffer);
+    if (message == "END") break;
+    Console.WriteLine(message);
+}
+
+udpClient.DropMulticastGroup(broadcastAddress);
+Console.WriteLine("Udp-клиент завершил свою работу");
---
+++ Multicast_Demo.Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+ 
+var messages = new[] { "Hello World!", "Hello METANIT.COM", "Hello work", "END" };
+var broadcastAddress = IPAddress.Parse("235.5.5.11");
+using var udpSender = new UdpClient();
+Console.WriteLine("Начало отправки сообщений...");
+foreach(var message in messages)
+{
+    Console.WriteLine($"Отправляется сообщение: {message}");
+    var data = Encoding.UTF8.GetBytes(message);
+    await udpSender.SendAsync(data, new IPEndPoint(broadcastAddress, 8001));
+    await Task.Delay(1000);
+}
